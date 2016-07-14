51d185619f37a828dcfb25b5d220f78a608ca633
David Przybilla <dav.alejandro@gmail.com>
2016-07-14 16:55:59 +0700

    formulario basico de solicitud de datasets

--- ui.R
+++ ui.R
@@ -9,65 +9,134 @@
 
 library(shiny)
 
+
+formulario_solicitante <- column(12, h3("Informacion sobre el solicitante del dataset"),
+                                     
+                                     textInput("text", label = h4("Nombre"), 
+                                               value = "Enter text..."),
+                                     
+                                     textInput("text", label = h4("Apellidos"), 
+                                               value = "Enter text..."),
+                                     
+                                     textInput("text", label = h4("Tipo Identificacion"), 
+                                               value = "Enter text..."),
+                                     
+                                     textInput("text", label = h4("Identificacion"), 
+                                               value = "Enter text..."),
+                                     
+                                     textInput("text", label = h4("Telefono"), 
+                                               value = "Enter text..."),
+                                     
+                                     textInput("text", label = h4("Email"), 
+                                               value = "Enter text..."),
+                                     
+                                     
+                                     actionButton("derecho","Crear"))
+
+formulario_institucion <- column(12, h3("Informacion sobre la institucion que administra el dataset"),
+                                     
+                                     textInput("text", label = h4("Nombre de la Entidad"), 
+                                               value = "Enter text..."),
+                                     
+                                     textInput("text", label = h4("Ciudad"), 
+                                               value = "Enter text..."),
+                                     
+                                     textInput("text", label = h4("Direccion"), 
+                                               value = "Enter text...")
+                                 )
+formulario_funcionario <- column(12, h3("Informacion sobre la institucion que administra el dataset"),
+                                 
+                                 # agregar opcion para permitir remitir el template a un funcionario anonimo 
+                                 # en caso que el usuario no conozca el nombre del funcionario
+                                 textInput("text", label = h4("Nombre del funcionario encargado"), 
+                                           value = "Enter text..."),
+                                 
+                                 textInput("text", label = h4("Apellidos del funcionario encargado"), 
+                                           value = "Enter text..."),
+                                 
+                                 textInput("text", label = h4("Cargo del Funcionario"), 
+                                           value = "Enter text...")
+                                 
+                          )
+
+formulario_dataset <- column(12, h3("Informacion sobre la institucion que administra el dataset"),
+                                 
+                                 # agregar opcion para permitir remitir el template a un funcionario anonimo 
+                                 # en caso que el usuario no conozca el nombre del funcionario
+                                 textInput("text", label = h4("Nombre del funcionario encargado"), 
+                                           value = "Enter text..."),
+                                 
+                                 textInput("text", label = h4("Apellidos del funcionario encargado"), 
+                                           value = "Enter text..."),
+                                 
+                                 textInput("text", label = h4("Cargo del Funcionario"), 
+                                           value = "Enter text...")
+                                 
+)
+
+formulario_dataset <- column(12, h3("Informacion sobre la institucion que administra el dataset"),
+                             
+                             # agregar opcion para permitir remitir el template a un funcionario anonimo 
+                             # en caso que el usuario no conozca el nombre del funcionario
+                             textInput("text", label = h4("Descripcion del Dataset/datos/informacion a la que desea acceder"), 
+                                       value = "Enter text..."),
+                             
+                             textInput("text", label = h4("Especifique que campos en particular del dataset le son indispensables "), 
+                                       value = "Enter text..."),
+                             
+                             textInput("text", label = h4("Especifique el rango preferible de tiempo del dataset que solicita"), 
+                                       value = "Enter text...")
+                      )
+
+restricciones_dataset <- column(12, h3("Privacidad"),
+                             
+                        
+                             checkboxInput("checkbox", label = h4("El dataset podria contener campos que puedan vulnerar el derecho a la privacidad de terceros"), 
+                                          value = FALSE),
+                             
+                             textInput("text", label = h4("(en caso de si)Escriba cuales campos que podrian afectar la privacidad de 3eros no son indispensables para usted"), 
+                                       value = "Enter text...")
+)
+
+clasificacion_dataset <- column(12, h3("Clasificacion"),
+                                
+                              
+                                checkboxInput("checkbox", label = h4("El dataset contiene datos amparados como `informacion clasificada`? "), 
+                                              value = FALSE),
+                                checkboxInput("checkbox", label = h4("Ha pasado mas de 30anos desde su clasificacion?"), 
+                                              value = FALSE)
+                        )
+
+otras_restricciones <- column(12, h3("El dataset contiene:"),
+                   
+                              checkboxInput("checkbox", label = h4("datos acerca de secretos industriales ? "), 
+                                            value = FALSE),
+                              checkboxInput("checkbox", label = h4("datos acerca de secretos de seguridad nacional?"), 
+                                            value = FALSE),
+                              checkboxInput("checkbox", label = h4("datos acerca de secretos de relaciones internacionles?"), 
+                                            value = FALSE),
+                              checkboxInput("checkbox", label = h4("datos resultantes de investigaciones judiciales en desarrollo"), 
+                                            value = FALSE)
+)
+
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
+shinyUI(
+  
+  fluidPage(
+    
+    titlePanel("Solicitud de Datos Abiertos - Primera Solicitud "),
   
   # INIT SELECTION
   fluidRow(
-    column(4,
-           icon("calendar"),
-           h1("Derecho de petición"),
-           actionButton("derecho","Crear")
-    ),
-    column(4,
-           icon("calendar"),
-           h1("Reposición"),
-           actionButton("reposicion","Crear")
-    ),
-    column(4,
-           icon("calendar"),
-           h1("Tutela"),
-           actionButton("tutela","Crear")
-    )
-  ),
-  fluidRow(id="derechoRow",
-           h1("Crear Derecho de Petición"),
-    sidebarLayout(
-      sidebarPanel(
-        uiOutput("derechoControls")
-        
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-        plotOutput("distPlot")
-      )
-    )
-  ),
-  fluidRow(id="reposicionRow",
-           h1("Crear Reposicion"),
-           sidebarLayout(
-             sidebarPanel(
-               uiOutput("reposicionControls")
-             ),
-             
-             # Show a plot of the generated distribution
-             mainPanel(
-               plotOutput("distPlot")
-             )
-           )
-  ),
-  fluidRow(id="tutelaRow",
-           h1("Crear Tutela"),
-           sidebarLayout(
-             sidebarPanel(
-               uiOutput("tutelaControls")
-             ),
-             
-             # Show a plot of the generated distribution
-             mainPanel(
-               plotOutput("distPlot")
-             )
-           )
+    
+    fluidPage( navlistPanel(
+      tabPanel("Informacion Solicitante", formulario_solicitante),
+      tabPanel("Institucion", formulario_institucion),
+      tabPanel("Funcionario Encargado", formulario_funcionario),
+      tabPanel("Dataset", formulario_dataset),
+      tabPanel("Privacidad", restricciones_dataset),
+      tabPanel("Clasificacion", clasificacion_dataset),
+      tabPanel("Otras restricciones", otras_restricciones)))
   )
+  
 ))
