6a12e5035d5f2dfb72317921108da929f4936ed0
CamilaAchury <camilaachury.4@gmail.com>
2017-07-10 15:01:06 -0500

    added app queremos dats with steps

--- server.R
+++ server.R
@@ -1,26 +1,157 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(shinyjs)
+library(tidyverse)
+library(shinysteps)
+library(bsplus)
+library(pystr)
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
+server <- function(input,output,session){
+
+  
+  output$info_gen <- renderUI({
+    info <- list(
+                      textInput("sol_nombre", label = "Nombre", value = ""),
+                      textInput("sol_apellidos", label = "Apellidos",value = ""),
+                      textInput("sol_cargo", label = "Cargo",value = ""),
+                      #fluidRow(
+                     div(class = "row",
+                         div(class = "app-button",
+                      selectizeInput("sol_tipo_id", label = "Doc.", 
+                                      c("T.I", "C.C", "C.E", "NIT."), width = "62px")
+                        ),
+                      div(class = "app-button",
+                      textInput("sol_idNum", label = "N. de identidad", 
+                                                    value = "", width = "234px")
+                        )),
+                     
+                     textInput("sol_direccion", label = "Dirección"),
+                     # textInput("sol_tel", label = "Telefono"),
+                     textInput("sol_email", label = "Email")
+                                )
+  info
     
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+  }) 
+  
+
+  output$info_inst <- renderUI({
+    info <- list(
+      
+                     textInput("ent_ciudad", label = "Ciudad"),
+                     textInput("ent_nombre", label = "Nombre de la Entidad")
+                     
+                     )
     
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    info                                                                
+    })
+  
+  
+  output$info_data <- renderUI({
+    info <- list(
+                     textInput('data_ciudad', 'Ciudad de la cual realiza la consulta'),
     
+                     textInput('data_int', 'Descripción de la información que desea obtener'),
+    
+                     checkboxInput('info_reserva', 'La información solicitada es clasificada o de reserva')
+    )
+    info
   })
   
-})
+  
+ 
+  output$val_num <- renderText({ input$sol_idNum})
+  output$val_tel <- renderText({ input$sol_tel})
+  output$val_eml <- renderText({ input$sol_email})
+  
+
+  output$nombre <- renderText({
+    paste(input$sol_nombre, input$sol_apellidos)
+  })
+  
+  output$entidad <- renderText({
+    input$ent_nombre
+  })
+  
+  template <- reactive({
+   
+    templ <- list(
+      fluidRow(
+        div(id = "headsty",
+          HTML(
+            paste0(input$data_ciudad, ", ", 'fecha', '</br></br>',
+                   textOutput('nombre'), input$sol_cargo, textOutput('entidad'), input$ent_ciudad,
+                  '</br></br>', 'Referencia: Solicitud de información </br> Respetado señor/a: </br></br> 
+                 <p style="text-align: justify;"> En ejercicio del derecho fundamental de petición, consagrado en el artículo 23 de la 
+                  Constitución Nacional, y del derecho de acceso a la información pública, consagrado en los artículos 20 y 74 de la misma, desarrollado por 
+                  la ley 1712 de 2014; de manera respetuosa le solicito la siguiente información:</p></br>',
+                   '<p style="text-align: justify;">',input$data_int, '</p>',
+                   '<p style="text-align: justify;"> La respuesta a la presente solicitud la recibiré en la ', input$sol_direccion,'o en mi correo electrónico', 
+                   input$sol_email, '.</p>', '<p> Cordialmente, </p> </br>','
+                  <div class="firmLine">', paste(input$sol_nombre, input$sol_apellidos), '
+                  </div>
+                  <p>', paste0(input$sol_tipo_id, " ",input$sol_idNum) ,'</p>
+                  '
+                  
+            )
+          ),
+          div(align = 'center',
+              downloadButton('descarga_pet', 'Descargar PDF')  
+              
+          )
+      )
+    )
+    )
+    
+    templ
+    
+    
+    })
+  
+  
+  
+  observeEvent(input$show, {
+    showModal(modalDialog(
+      title = "",
+      template(),
+      easyClose = TRUE,
+      footer = NULL
+    ))
+  })
+  
+  
+  output$descarga_pet <- 
+    downloadHandler(
+      "peticion.pdf",
+      content = 
+        function(file)
+        {
+          params <- list(
+            ciudad = input$data_ciudad,
+            fecha = '16 de septiembre 2341',
+            nombres = paste(input$sol_nombre, input$sol_apellidos),
+            cargo = input$sol_cargo,
+            entidad = input$ent_nombre,
+            ciudadDat = input$ent_ciudad,
+            info = input$data_int,
+            direc = input$sol_direccion,
+            correo = input$sol_email,
+            documento = paste0(input$sol_tipo_id, " ",input$sol_idNum))
+          rmarkdown::render("temp_latex/untitle.Rmd",
+                            #output_format = pdf_document(template="default.tex"),
+                            params = params,
+                            output_file = "peticion_gen.pdf")
+          
+          readBin(con = "temp_latex/peticion_gen.pdf", 
+                  what = "raw",
+                  n = file.info("temp_latex/peticion_gen.pdf")[, "size"]) %>%
+            writeBin(con = file)
+        },
+      contentType = 'temp_latex/peticion_gen.pdf'
+    )
+  
+  
+  
+  
+
+}
+
+
--- ui.R
+++ ui.R
@@ -1,135 +1,80 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(shinysteps)
+library(shinyjs)
+library(tidyverse)
+library(bsplus)
 
+styles <- "
 
-formulario_solicitante <- column(12, h3("Informacion sobre el solicitante del dataset"),
-                                     
-                                     textInput("solitante_nombre", label = p("Nombre"), 
-                                               value = ""),
-                                     
-                                     textInput("solicitante_apellidos", label = p("Apellidos"), 
-                                               value = ""),
-                                     
-                                     textInput("solicitante_tipo_identificacion", label = p("Tipo Identificacion"), 
-                                               value = ""),
-                                     
-                                     textInput("solicitante_identificacion", label = p("Identificacion"), 
-                                               value = ""),
-                                     
-                                     textInput("solicitante_telefono", label = p("Telefono"), 
-                                               value = "Enter text..."),
-                                     
-                                     textInput("solicitante_email", label = p("Email"), 
-                                               value = "")
-                                 )
+#headsty{
+max-width: 100%;
+margin-top: 2%;
+margin-left: 10%;
+margin-right: 10%;
+}
 
-formulario_institucion <- column(12, h3("Informacion sobre la Entidad que administra el dataset"),
-                                     
-                                     textInput("entidad_nombre", label = p("Nombre de la Entidad"), 
-                                               value = ""),
-                                     
-                                     textInput("entidad_ciudad", label = p("Ciudad"), 
-                                               value = ""),
-                                     
-                                     textInput("entidad_direccion", label = p("Direccion"), 
-                                               value = "")
-                                 )
-formulario_funcionario <- column(12, h3("Informacion sobre el funcionario al que dirige la peticion"),
-                                 
-                                 # agregar opcion para permitir remitir el template a un funcionario anonimo 
-                                 # en caso que el usuario no conozca el nombre del funcionario
-                                 textInput("funcionario_nombre", label = p("Nombre del funcionario"), 
-                                           value = ""),
-                                 
-                                 textInput("funcionario_apellido", label = p("Apellidos del funcionario"), 
-                                           value = ""),
-                                 
-                                 textInput("funcionario_cargo", label = p("Cargo del funcionario"), 
-                                           value = "")
-                                 
-                          )
+.app-button{
+    display:inline-block;
+}
+.row{
+  text-align:left;
+  margin-left:0px;
+}
+
+.firmLine{     
+  text-decoration:overline;
+  color : black;
+}
+
+.firmResultat{
+  color : black;
+}
+
+#sidebar_step2 {
+color : green;
+border: solid red;
+background-color: red;
+}
 
 
 
-formulario_dataset <- column(12, h3("Informacion sobre el dataset"),
-                             
-                             # agregar opcion para permitir remitir el template a un funcionario anonimo 
-                             # en caso que el usuario no conozca el nombre del funcionario
-                             textInput("dataset_descripcion", label = p("Descripcion del dataset/informacion a la que desea acceder"), 
-                                       value = ""),
-                             
-                             textInput("dataset_campos", label = p("Especifique que campos en particular del dataset le son indispensables "), 
-                                       value = ""),
-                             
-                             dateRangeInput('dataset_fecha',
-                                            label = p("Especifique el rango preferible de tiempo del dataset que solicita"),
-                                            start = Sys.Date() - 2, end = Sys.Date() + 2
-                             )
-                             
-                      )
+"
 
-restricciones_dataset <- column(12, h3("Privacidad"),
-                             
+stepsPage(styles = styles,
+          stepsHeader(show = FALSE, height = 150,
+                      br()
+           ),
+    stepsBody(initStep = "step1",
+            stepPanel(id="step1",
+                      
+                      sideBarStep(title = "PASO 1"#,
+                                  #p("Datos personales")
+                      ),
+                      
+                      mainStep(
+                        h4("Información sobre el solicitante"),
+                        #br(),
+                        #textInput("sol_nombre", label = "Nombre", value = ""),
+                        uiOutput("info_gen")
                         
-                             checkboxInput("privacidad_violacion_flag", label = p("El dataset podria contener campos que puedan vulnerar el derecho a la privacidad de terceros"), 
-                                          value = FALSE),
-                             
-                             textInput("campos_violan_privacidad", label = p("(en caso de si)Escriba cuales campos que podrian afectar la privacidad de 3eros no son indispensables para usted"), 
-                                       value = "")
+                      )
+            ),
+            stepPanel(id="step2",
+                      sideBarStep(title = "PASO DOS"
+                      ),
+                      mainStep(
+                        h4("Datos sobre la entidad y/o funcionario al que se solicita la información"),
+                        uiOutput("info_inst")
+                      )
+            ),
+            stepPanel(id="step3",
+                      sideBarStep(title = "PASO TRES"
+                      ),
+                      mainStep(
+                        h4("Datos sobre la información requerida"),
+                        uiOutput("info_data"),
+                        actionButton("show", "Vista previa")
+                      )
+            )
+  )
 )
-
-clasificacion_dataset <- column(12, h3("Clasificacion"),
-                                
-                              
-                                checkboxInput("clasificacion_flag", label = p("El dataset contiene datos amparados como `informacion clasificada`? "), 
-                                              value = FALSE),
-                                checkboxInput("clasificacion_expiracion_flag", label = p("Ha pasado mas de 30anos desde su clasificacion?"), 
-                                              value = FALSE)
-                        )
-
-otras_restricciones <- column(12, h3("El dataset contiene:"),
-                   
-                              checkboxInput("restriccion_secreto_industrial_flag", label = p("datos acerca de secretos industriales ? "), 
-                                            value = FALSE),
-                              checkboxInput("restriccion_seguridad_nacional_flag", label = p("datos acerca de secretos de seguridad nacional?"), 
-                                            value = FALSE),
-                              checkboxInput("restriccion_relaciones_internacionales_flag", label = p("datos acerca de secretos de relaciones internacionles?"), 
-                                            value = FALSE),
-                              checkboxInput("restriccion_investigacion_judicial", label = p("datos resultantes de investigaciones judiciales en desarrollo"), 
-                                            value = FALSE)
-)
-
-# Define UI for application that draws a histogram
-shinyUI(
-  
-  fluidPage(
-    
-    titlePanel("Solicitud de Datos Abiertos - Primera Solicitud "),
-  
-  # INIT SELECTION
-  fluidRow(
-    
-    fluidPage( navlistPanel(
-      tabPanel("Informacion Solicitante", formulario_solicitante),
-      tabPanel("Institucion", formulario_institucion),
-      tabPanel("Funcionario Encargado", formulario_funcionario),
-      tabPanel("Dataset", formulario_dataset),
-      tabPanel("Privacidad", restricciones_dataset),
-      tabPanel("Clasificacion", clasificacion_dataset),
-      tabPanel("Otras restricciones", otras_restricciones),
-      tabPanel(submitButton("Generar Solicitud")))
-  ))
-  
-))
-
-
-
-
